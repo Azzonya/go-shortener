0a85725078873c644a79e4bf0882dbb803038793
sdk17 <zw2793@gmail.com>
2023-11-01 11:40:00 +0600

    commit7

--- internal/adapters/server/handlers.go
+++ internal/adapters/server/handlers.go
@@ -24,7 +24,7 @@ func (o *Rest) HShortener(w http.ResponseWriter, r *http.Request) {
 
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		http.Error(w, "Error reading request body", http.StatusCreated)
 		return
 	}
 
