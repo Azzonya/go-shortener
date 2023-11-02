2e7e3f7cef70d205ab9a47a8da3dcc38f552d6ee
sdk17 <zw2793@gmail.com>
2023-11-02 16:12:31 +0600

    iter2

--- internal/adapters/server/handlers.go
+++ internal/adapters/server/handlers.go
@@ -30,7 +30,7 @@ func (o *Rest) HShortener(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusCreated)
-	fmt.Fprint(w, fmt.Sprintf("http://localhost:8080/"+shortURL))
+	fmt.Fprint(w, "http://localhost:8080/"+shortURL)
 }
 
 func (o *Rest) HRedirect(w http.ResponseWriter, r *http.Request) {
