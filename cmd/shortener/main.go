046a73b76883e1014ca8f6280bb9e8d813f7b500
Azzonya <azamat.beisembinov@gmail.com>
2024-05-13 13:43:00 +0500

    iter20 - start ldflags

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Azzonya/go-shortener/internal/app"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
+	fmt.Println("Build version:", getValueOrDefault(buildVersion))
+	fmt.Println("Build date:", getValueOrDefault(buildDate))
+	fmt.Println("Build commit:", getValueOrDefault(buildCommit))
+
 	app.Start()
+}
+
+func getValueOrDefault(value string) string {
+	if value == "" {
+		return "N/A"
+	}
+	return value
 }
